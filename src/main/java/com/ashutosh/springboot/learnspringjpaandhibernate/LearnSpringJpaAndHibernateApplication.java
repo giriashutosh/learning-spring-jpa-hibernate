3626d34fc20ba9b5d38ff0ea0819b62b7477ecfa
Ashutosh Giri <ashutoshgiri249@gmail.com>
2024-01-12 12:13:08 +0530

    start jpa

---
+++ src/main/java/com/ashutosh/springboot/learnspringjpaandhibernate/LearnSpringJpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.ashutosh.springboot.learnspringjpaandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringJpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringJpaAndHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashutosh/springboot/learnspringjpaandhibernate/LearnSpringJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashutosh.springboot.learnspringjpaandhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
